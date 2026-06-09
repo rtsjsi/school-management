2c1be46c1c4de0dc6b8c71684e0412ee6399116d
rtsjsi <rtsjsi@yahoo.com>
2026-06-09 15:27:50 +0530

    UI Tweaks: fix Create User button size in form

--- src/components/CreateUserForm.tsx
+++ src/components/CreateUserForm.tsx
@@ -108,8 +108,8 @@ export default function CreateUserForm() {
           ))}
         </select>
       </div>
-      <div className="flex justify-start">
-      <SubmitButton loading={loading} loadingLabel="Creating…">
+      <div className="flex justify-end pt-2">
+      <SubmitButton loading={loading} loadingLabel="Creating…" className="w-full h-10 px-4 py-2 text-sm">
         Create user
       </SubmitButton>
     </div>
